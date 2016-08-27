b28f46a308cfeacded244a24ba8993e5b8ae0284
Oleg G.Kapranov <oleg@vertiopia.com>
2016-08-27 11:16:55 +0300

    Added object to my-component.js by component

--- app/components/my-component.js
+++ app/components/my-component.js
@@ -2,5 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   myProp: 100,
-  myService: Ember.inject.service()
+  myService: Ember.inject.service(),
+  myNewName: Ember.inject.service('myService')
 });
